692f5d4b710b0af48c14ef60fc0cfbee7f9dce1f
Leo <leonardlevasseur@yahoo.com>
2022-11-22 16:30:14 +0100

    some stable thing

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 09:21:50 by levasse           #+#    #+#             */
+/*   Updated: 2022/11/22 13:01:55 by leo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char    *get_next_line(int fd);
+void    fill_char(char *dst, char *src);
+
+#endif
